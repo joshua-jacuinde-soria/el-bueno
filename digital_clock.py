5e213f9bb87adf6260e2aef397bd0d170b09db10
Joshua Soria <joshua.jacuinde@cua.uam.mx>
2024-05-08 13:24:09 -0600

    creacion del metodo de incremento

--- digital_clock.py
+++ digital_clock.py
@@ -28,4 +28,10 @@ class Digital_clock:
         self.__h, self.__m, self.__m = 0, 0, 0
         
     def incremento(self):
-        
+        """incremento de un segundo en tiempo concurrente
+        """
+        self.__h = self.__h + 1 if self.__m ==59 and self.__s == 59 else self.__h
+        self.__h = 0 if self.__h == 24 else self.__h
+        self.__m = self.__m + 1 if self.__s == 59 else self.__m
+        self.__m = 0 if self.__m == 60 else self.__m
+        self.__s = self.__s + 1 if self.__s < 59 else 0
