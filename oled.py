fcd0692f8b66f0828d39c03b5cff95113dec42d9
Joshua Soria <joshua.jacuinde@cua.uam.mx>
2024-05-08 13:45:01 -0600

    pruebas con oled

--- oled.py
+++ oled.py
@@ -1,3 +1,10 @@
 from machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
 
 i2c = I2C(scl = Pin(8), sda = Pin(9), id = 0)
+oled = SSD1306_I2C(128, 32, i2c)
+
+def oled_pruebas(tiempo):
+    oled.text('Tiempo', 80, 0)
+    oled.text(tiempo, 50, 10)
+    oled.show()
