75880983031a93cecfd1d53ed4932c10a7474f71
Joshua Soria <joshua.jacuinde@cua.uam.mx>
2024-05-08 13:50:43 -0600

    pruebas con oled

--- oled.py
+++ oled.py
@@ -1,10 +1,14 @@
 from machine import Pin, I2C
 from ssd1306 import SSD1306_I2C
+from utime import sleep
 
-i2c = I2C(scl = Pin(8), sda = Pin(9), id = 0)
+
+i2c = I2C(sda = Pin(8), scl = Pin(9), id = 0)
 oled = SSD1306_I2C(128, 32, i2c)
 
 def oled_pruebas(tiempo):
-    oled.text('Tiempo', 80, 0)
-    oled.text(tiempo, 50, 10)
+    oled.text('Tiempo', 50, 0)
+    oled.text(tiempo, 20, 10)
     oled.show()
+    sleep(1)
+    
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from digital_clock import Digital_clock
 from utime import sleep
+from oled import oled_pruebas
 
 def clock_test():
     clock = Digital_clock(23, 59, 40)
@@ -7,5 +8,6 @@ def clock_test():
         h, m, s = clock.get_time()
         time = f'{h:02} : {m:02} : {s:02}'
         print(time)
+        oled_pruebas(time)
         sleep(1)
         clock.incremento()
